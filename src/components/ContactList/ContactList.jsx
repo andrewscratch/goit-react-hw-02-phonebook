c1c012181606fb05bead814f70d7318ffb6ce425
andrewscratch <andrew.chudlya@gmail.com>
2023-02-04 20:03:01 +0200

    phonebook hw
    phonebook hw

---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,24 @@
+import {
+  ContactsList,
+  ContactItem,
+  DeleteBtn,
+  ContactName,
+} from './ContactList.styled';
+
+export const ContactList = ({ visibleContacts, onDeleteContact }) => {
+  return (
+    <ContactsList>
+      {visibleContacts.map(({ id, name, number }) => {
+        return (
+          <ContactItem key={id}>
+            <span>&#128241;</span>
+            <div>
+              <ContactName>{name}</ContactName> : {number}
+            </div>
+            <DeleteBtn onClick={() => onDeleteContact(id)}>Delete</DeleteBtn>
+          </ContactItem>
+        );
+      })}
+    </ContactsList>
+  );
+};
